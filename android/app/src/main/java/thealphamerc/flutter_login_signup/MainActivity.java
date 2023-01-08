bdc2d21a5effc82ebfbb63bb1b25a37426fcdaf5
eslam733 <emohamef733@gmail.com>
2023-01-08 14:03:27 +0200

    migration v2

--- android/app/src/main/java/thealphamerc/flutter_login_signup/MainActivity.java
+++ android/app/src/main/java/thealphamerc/flutter_login_signup/MainActivity.java
@@ -1,13 +1,15 @@
 package thealphamerc.flutter_login_signup;
-
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+       private static final String CHANNEL = "samples.flutter.dev/battery";
+
+  // @Override
+  // public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+  //   GeneratedPluginRegistrant.registerWith(flutterEngine);
+  // }
+
+      
 }
